1f23cf442365130e541af4d5fae4c202ed1f7fad
krishnatejakotla <krishnatejareddy.kotla@gmail.com>
2025-08-22 19:31:35 -0500

    Delete glue/athena/athena/10_monthly_revenue.sql

--- glue/athena/athena/10_monthly_revenue.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  date_trunc('month', CAST(invoicedate AS timestamp)) AS month,
-  SUM(CAST(quantity AS double) * CAST(unitprice AS double)) AS total_revenue
-FROM default.retail_sales_cleaned
-WHERE quantity IS NOT NULL AND unitprice IS NOT NULL
-GROUP BY 1
-ORDER BY 1;
