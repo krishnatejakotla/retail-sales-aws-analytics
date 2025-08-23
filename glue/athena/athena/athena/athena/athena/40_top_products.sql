ed16956cfd53c842531b740dfb30b30a3717f835
krishnatejakotla <krishnatejareddy.kotla@gmail.com>
2025-08-22 19:32:29 -0500

    Delete glue/athena/athena/athena/athena/athena/40_top_products.sql

--- glue/athena/athena/athena/athena/athena/40_top_products.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  description,
-  SUM(CAST(quantity AS double) * CAST(unitprice AS double)) AS total_revenue
-FROM default.retail_sales_cleaned
-WHERE description IS NOT NULL
-  AND quantity IS NOT NULL AND unitprice IS NOT NULL
-GROUP BY description
-ORDER BY total_revenue DESC
-LIMIT 10;
